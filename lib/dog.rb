758ed0ce6f025d69977ea6f53fc0e82b8b405797
Vincent Makokha <makokhavomondi@gmail.com>
2022-10-28 16:25:09 +0300

    Completed Assignment (Everything plus bonus)

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,8 +41,8 @@ class Dog
     end
 
     def self.create(name:, breed:, id:nil)
-        song = self.new(name:name, breed:breed, id:id)
-        song.save
+        dog = self.new(name:name, breed:breed, id:id)
+        dog.save
     end
 
     def self.new_from_db(row)
@@ -60,14 +60,39 @@ class Dog
     end
 
     def self.find_by_name(name)
-        all.find do |song|
-            song.name == name
+        all.find do |dog|
+            dog.name == name
         end
     end
 
     def self.find(id)
-        all.find do |song|
-            song.id == id
+        all.find do |dog|
+            dog.id == id
+        end
+    end
+
+    def self.find_or_create_by(name:, breed:, id:nil)
+        dog = get_dog(name:name, breed:breed)
+
+        if dog
+            dog
+        else
+            self.create(name:name, breed:breed, id:id)
+        end
+    end
+
+    def update
+        sql = <<-SQL
+            UPDATE dogs SET name=? WHERE id=?
+        SQL
+
+        DB[:conn].execute(sql, self.name, self.id)
+    end
+
+    private
+    def self.get_dog(name:, breed:)
+        all.find do |dog|
+            dog.name == name && dog.breed == breed
         end
     end
 end
