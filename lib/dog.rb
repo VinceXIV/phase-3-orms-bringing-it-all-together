6917c93d7ce07c19e022d5ec78f1cf16fc8bd8ef
Vincent Makokha <makokhavomondi@gmail.com>
2022-10-28 16:02:37 +0300

    Complete the Core deliverables

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,73 @@
 class Dog
 
+    attr_accessor :name, :breed, :id
+
+    def initialize(name:, breed:, id: nil)
+        @name = name
+        @breed = breed
+        @id = id
+    end
+
+    def self.create_table
+        sql = <<-SQL
+            CREATE TABLE dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            );
+        SQL
+
+        DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        sql = <<-SQL
+            DROP TABLE dogs;
+        SQL
+
+        DB[:conn].execute(sql)
+    end
+
+    def save
+        sql = <<-SQL
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?);
+        SQL
+
+        DB[:conn].execute(sql, self.name, self.breed)
+        self.id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
+
+        self
+    end
+
+    def self.create(name:, breed:, id:nil)
+        song = self.new(name:name, breed:breed, id:id)
+        song.save
+    end
+
+    def self.new_from_db(row)
+        self.new(id:row[0], name:row[1], breed:row[2])
+    end
+
+    def self.all
+        sql = <<-SQL
+            SELECT * FROM dogs;
+        SQL
+
+        DB[:conn].execute(sql).map do |row|
+            new_from_db(row)
+        end
+    end
+
+    def self.find_by_name(name)
+        all.find do |song|
+            song.name == name
+        end
+    end
+
+    def self.find(id)
+        all.find do |song|
+            song.id == id
+        end
+    end
 end
